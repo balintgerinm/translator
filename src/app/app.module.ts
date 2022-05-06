535e080f93d09a8c13b8c0f31526898e6c9ebcf5
Gergő Bálint <gergo.balint@dynazol.com>
2022-05-06 10:59:08 +0200

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslateComponent } from './components/translate/translate.component';
+import { SynonymComponent } from './components/synonym/synonym.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TranslateComponent,
+    SynonymComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
